1f50ba650117a76a1bf16882d21fa922163a407b
Kris <162702144+KRISLEMA@users.noreply.github.com>
2025-03-24 13:36:38 +0300

    add sidebar && navbar dir

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 // import { useAppDispatch, useAppSelector } from "@/app/redux";
 // import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 import { Bell, Menu, Sun, Moon, Settings } from 'lucide-react';
@@ -8,19 +10,19 @@ import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
-//     const dispatch = useAppDispatch();
-//     const isSidebarCollapsed = useAppSelector(
-//         (state) => state.global.isSidebarCollapsed
-//     );
-//     const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-//     const toggleSidebar = () => {
-//         dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-//     };
+    const toggleSidebar = () => {
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
 
-//     const toggleDarkMode = () => {
-//         dispatch(setIsDarkMode(!isDarkMode));
-//     };
+    const toggleDarkMode = () => {
+        dispatch(setIsDarkMode(!isDarkMode));
+    };
 
     return (
         <div className="flex justify-between items-center w-full mb-7">
@@ -28,7 +30,7 @@ const Navbar = () => {
           <div className="flex justify-between items-center gap-5">
             <button
               className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-              onClick={() => {}}
+              onClick={toggleSidebar}
             >
               <Menu className="w-4 h-4" />
             </button>
